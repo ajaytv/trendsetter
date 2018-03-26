7cfab3ecc0108f77cf7f4e10a8df0b588873c440
Ajay Venkateswaran <ajay.tv1@gmail.com>
2018-03-26 14:55:34 +0530

    Literature history

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 import { PopoverController, NavParams } from 'ionic-angular';
-
 import { PopoverPage } from '../about-popover/about-popover';
-
+import { InAppBrowser } from '@ionic-native/in-app-browser';
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
@@ -11,11 +10,20 @@ export class AboutPage {
   public display:string = 'Summary plan.pdf';
   conferenceDate = '2047-05-17';
   public navparams:NavParams;
-  constructor(public popoverCtrl: PopoverController) {}
+  constructor(public popoverCtrl: PopoverController, public inAppBrowser:InAppBrowser) {}
   
   presentPopover(event: Event) {
     let popover = this.popoverCtrl.create(PopoverPage);
     popover.present({ ev: event });
+      }
+      openPdf()
+      {
+       
+          this.inAppBrowser.create(
+            `http://kmmc.in/wp-content/uploads/2014/01/lesson2.pdf`,
+            '_blank'
+          );
+        
       }
   
 }
